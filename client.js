01d8ab1f002d73f5f34e54c3b3abfc7e23aa31f0
Brenan Marenger <104685201+BrenanMarenger@users.noreply.github.com>
2022-11-06 13:24:00 -0500

    Update client.js

--- client.js
+++ client.js
@@ -2,17 +2,17 @@ function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect(); // abs. size of element
     scaleX = canvas.width / rect.width;    // relationship bitmap vs. element for x
     scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for y
-  
+
     return {
-      x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
-      y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+        x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+        y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
     }
-  }
+}
 
 let myApp = Vue.createApp({
     data() {
         return {
-            latitude: null, 
+            latitude: null,
             longitude: null,
             clickX: null,
             clickY: null,
@@ -52,16 +52,17 @@ let myApp = Vue.createApp({
                 .then(data => {
                     if (!data.properties || !data.properties.periods) throw new Error("No data for some reason.");
                     this.forecasts = data.properties.periods;
+                    console.log(this.forecasts);
                     this.results = "Success";
                 })
                 .catch(err => {
-                    this.results = err+"";
+                    this.results = err + "";
                 }
-            );
-            this.results = "Loading..."; 
+                );
+            this.results = "Loading...";
             this.forecasts = null;
-            },
-    positionCursor(event) {
+        },
+        positionCursor(event) {
             //Put a marker where they clicked.
             let position = getMousePos(event.srcElement, event);
             this.clickX = position.x;
